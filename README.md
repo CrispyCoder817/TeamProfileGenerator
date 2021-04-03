2ff2a76ac5621f4c4d2d1fa3cc918eb14140df9f
Brandon G.M <brandongmerritt@gmail.com>
2021-04-03 17:13:39 -0400

    final push

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # TeamProfileGenerator
-The app generates a card with specific information from a CLI Generator
+  ## Description
+  The project focused on generating Team Profiles file so the business at hand can keep an easy access record base for all the employees that work at the company.
+  
+  ## Table of Contents
+1. [Install](#install)
+2. [Usage](#usage)
+3. [License](#license)
+4. [Contributing](#contributing)
+5. [Tests](#tests)
+6. [Questions](#questions)
+  ## Install
+  You would npm install all the inquirer packages and node modules, but keep a git ignore so you dont spend a bunch of space on the files. Running node index.js with the files on your local machine is for best results. 
+  ## Usage
+  It generates a Team Profile Generaotr using the input you type in the CLI to generate a series of cards for the employees. In future development it would be ideal for contact cards instead of using a rolodex from 1985; regardless how precious contacts on paper are. The best bet with this would be implementing everyone's imperative info like that of an emergency contact card for the right situation; if they are CPR certified and the like. Any and all certs would be an ideal use of this application.
+  ## License
+  
+  ## Contributing
+  If you follow me on GitHub you are more than welcome to fork and contribue.
+  ## Tests
+  If you are having trouble with this step try looking for the end brackets because that seems to be my biggest mistake is not ending or having an exgtra one floating around. The number one reason this would not work is failing to install all the proper npm packages.
+  ## Questions
+  If you'd like to connect with the developer about ReadMeGenerator further email them at brandongmerritt@gmail.com 
+  gitHub at @CrispyCoder817
--- index.js
+++ index.js
@@ -1,42 +1,181 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const Engineer = require("./employmentjs/Engineer");
-const Intern = require("./employmentjs/Intern");
-const Manager = require("./employmentjs/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
 
 const employees = [];
 
 function initApplication() {
-    beginHtml();
-    positionCard();
+  beginHtml();
+  positionCard();
 }
 
 function positionCard() {
-    inquirer.prompt([{
+  inquirer
+    .prompt([
+      {
         message: "Enter team member's name",
-        name: "name"
-    },
+        name: "name",
+      },
 
-    {
+      {
         type: "list",
         message: "Select position of team member",
-        choices: [
-            "Engineer",
-            "Intern",
-            "Manager",
-            "Intern"
-        ],
-        name: "position"
-    },
+        choices: ["Engineer", "Intern", "Manager"],
+        name: "position",
+      },
 
-    {
+      {
         message: "Enter team member's identification",
-        name: "id"
-    },
+        name: "id",
+      },
 
-    {
+      {
         message: "Enter email address of team member",
-        name: "email"
-    }])
+        name: "email",
+      },
+    ])
 
-    // .then(function()
+    .then(function ({ name, role, id, email }) {
+      let positionInfoCard = "";
+      if (role === "Engineer") {
+        positionInfoCard = "GitHub username";
+      } else if (role === "Intern") {
+        positionInfoCard = "school name";
+      } else {
+        positionInfoCard = "office phone number";
+      }
+      inquirer
+        .prompt([
+          {
+            message: `Enter team member's ${positionInfoCard}`,
+            name: "positionInfoCard",
+          },
+          {
+            type: "list",
+            message: "Would you like to add more team members?",
+            choices: ["yes", "no"],
+            name: "extraMembers",
+          },
+        ])
+        .then(function ({ positionInfoCard, extraMembers }) {
+          let neophyte;
+          if (role === "Engineer") {
+            neophyte = new Engineer(name, id, email, positionInfoCard);
+          } else if (role === "Intern") {
+            neophyte = new Intern(name, id, email, positionInfoCard);
+          } else {
+            neophyte = new Manager(name, id, email, positionInfoCard);
+          }
+          employees.push(neophyte);
+          addHtml(neophyte).then(function () {
+            if (extraMembers === "yes") {
+              positionCard();
+            } else {
+              finishHtml();
+            }
+          });
+        });
+    });
+}
+
+function beginHtml() {
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title>Team Profile</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-light bg-light mb-5">
+            <span class="navbar-brand mb-0 h1 w-100 text-center">Team Profile</span>
+        </nav>
+        <div class="container">
+            <div class="column">`;
+    fs.writeFile('./dist/teammates.html', html, function(err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+    console.log("start");
+}
+
+function addHtml(personnel) {
+    return new Promise(function(resolve, reject) {
+        const name = personnel.getName();
+        const role = personnel.getRole();
+        const id = personnel.getId();
+        const email = personnel.getEmail();
+        let data = "";
+        if (role === "Engineer") {
+            const gitHub = personnel.getGithub();
+            data = `<div class="col-3">
+            <div class="card mx-auto mb-3" style="width: 12rem">
+            <h5 class="card-header">${name}<br /><br />Engineer</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email Address: ${email}</li>
+                <li class="list-group-item">GitHub: ${gitHub}</li>
+            </ul>
+            </div>
+        </div>`;
+        } else if (role === "Intern") {
+            const school = personnel.getSchool();
+            data = `<div class="col-3">
+            <div class="card mx-auto mb-3" style="width: 12rem">
+            <h5 class="card-header">${name}<br /><br />Intern</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email Address: ${email}</li>
+                <li class="list-group-item">School: ${school}</li>
+            </ul>
+            </div>
+        </div>`;
+        } else {
+            const officePhone = personnel.getOfficeNumber();
+            data = `<div class="col-3">
+            <div class="card mx-auto mb-3" style="width: 12rem">
+            <h5 class="card-header">${name}<br /><br />Manager</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email Address: ${email}</li>
+                <li class="list-group-item">Office Phone: ${officePhone}</li>
+            </ul>
+            </div>
+        </div>`
+        }
+        console.log("produced +1 team member");
+        fs.appendFile('./dist/teammates.html', data, function (err) {
+            if (err) {
+                return reject(err);
+            };
+            return resolve();
+        });
+    });
+    
+            
+    
+        
+    
+    
+}
+
+function finishHtml() {
+    const html = ` </div>
+    </div>
+    
+</body>
+</html>`;
+
+    fs.appendFile('./dist/teammates.html', html, function (err) {
+        if (err) {
+            console.log(err);
+        };
+    });
+    console.log("fin");
+}
+initApplication();
